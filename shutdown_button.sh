62c7c60e5039f19e6c8d25ed79e003a615c6d8bc
CytronTH <89472682+CytronTH@users.noreply.github.com>
2022-01-16 13:14:29 +0700

    Create shutdown_button.sh

---
+++ shutdown_button.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+case "$1" in
+start)
+echo "Starting shutdown process"
+/usr/local/bin/shutdown_button.py &
+;;
+stop)
+echo "Stopping shutdown process"
+pkill -f /usr/local/bin/shutdown_button.py
+;;
+*)
+echo "Usage: /etc/init.d/shutdown_button.py {start|stop}"
+exit 1
+;;
+esac
